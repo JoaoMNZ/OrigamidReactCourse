4f739b95225621018be306100892edf868529482
JoaoMNZ <joao.menezes21@Outlook.com>
2025-02-04 08:27:22 -0300

    Hook useEffect exercise

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import React from 'react'
 import Produto from "./Produto"
 
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
 // https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+
 
 const App = () => {
   const [carregando, setCarregando] = React.useState(false);
   const [dados, setDados] = React.useState(null);
+
+  React.useEffect( () => {
+    const storageDatas = localStorage.getItem("produto");
+    storageDatas && fetchUrl(`https://ranekapi.origamid.dev/json/api/produto/${storageDatas}`)
+  }, []);
 
   async function fetchUrl (url){
     try {
       setCarregando(true)
       const resposta = await fetch(url);
       const dados = await resposta.json();
+      localStorage.setItem("produto", dados.id);
       setDados(dados);
     } catch (erro) {
       console.error("Erro ao buscar dados:", erro);
@@ -29,7 +34,7 @@ const App = () => {
 
   return (
     <section>
-      <button style={{margin: "0.5rem"}} onClick={ () => fetchUrl("https://ranekapi.origamid.dev/json/api/produto/tablet")}>Tablet</button>
+      <h1>Preferência: {localStorage.getItem("produto")}</h1>
       <button style={{margin: "0.5rem"}} onClick={ () => fetchUrl("https://ranekapi.origamid.dev/json/api/produto/smartphone")}>Smartphone</button>
       <button style={{margin: "0.5rem"}} onClick={ () => fetchUrl("https://ranekapi.origamid.dev/json/api/produto/notebook")}>Notebook</button>
       {carregando && <p>Carregando...</p>}
--- src/Produto.jsx
+++ src/Produto.jsx
@@ -5,7 +5,6 @@ const Produto = ({dados}) => {
     <div>
         <h1>{dados.nome}</h1>
         <p>R$ {dados.preco}</p>
-        {dados.fotos.map( (foto) => <img key={foto.titulo} src={foto.src} alt={foto.titulo} />)}
     </div>
   )
 }
