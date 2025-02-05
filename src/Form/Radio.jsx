2e42003becb9d1388d8564967a107f3ea3e4d08a
JoaoMNZ <joao.menezes21@Outlook.com>
2025-02-05 12:38:36 -0300

    Form challenge

--- src/Form/Radio.jsx
+++ src/Form/Radio.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React from 'react';
 
-const Radio = ({value, setValue, options, ...props}) => {
+const Radio = ({ pergunta, options, onChange, value, id, active }) => {
+  if (!active) return null;
   return (
-    options.map((option) => 
-    <label key={option}>
-        <input type="radio" value={option} checked={value === option} onChange={({target}) => setValue(target.value)} {...props}/>
-        {option}
-    </label>)
-  )
-}
+    <fieldset
+      style={{
+        padding: '2rem',
+        marginBottom: '1rem',
+        border: '2px solid #eee',
+      }}
+    >
+      <legend style={{ fontWeight: 'bold' }}>{pergunta}</legend>
+      {options.map((option) => (
+        <label
+          key={option}
+          style={{ marginBottom: '1rem', fontFamily: 'monospace' }}
+        >
+          <input
+            type="radio"
+            id={id}
+            checked={value === option}
+            value={option}
+            onChange={onChange}
+          />
+          {option}
+        </label>
+      ))}
+    </fieldset>
+  );
+};
 
-export default Radio
+export default Radio;
